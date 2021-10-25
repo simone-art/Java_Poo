8f041a54cb21671f0e5cfe800cc2203e29cd9fa2
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-10-25 20:30:46 -0300

    Criada classe usando a programação orientada a objeto

---
+++ src/beans/Livro.java
@@ -0,0 +1,12 @@
+package beans;
+
+public class Livro {
+	
+	//Especificar os atributos
+    //<modificadores> <dataType> <Atributos>
+	private String titulo;
+	private String editora;
+	private double valor;
+	private int anoPublicacao;
+	private int numeroPaginas;
+}
